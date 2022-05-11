3331faec1cf08480d76feb6f5a7e93221a58d273
AlenaBorodacheva <alien_borodach@mail.ru>
2022-05-11 23:28:00 +0300

    Добавлен сервис для работы с досками

--- TaskManager.Client/Services/CommonRequestService.cs
+++ TaskManager.Client/Services/CommonRequestService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
@@ -12,29 +13,61 @@ public abstract class CommonRequestService
 {
     public const string HOST = "http://localhost:52012/api/";
 
-    protected string GetDataByUrl(HttpMethod method, string url, AuthToken token, string userName = null, string password = null)
+    protected string GetDataByUrl(HttpMethod method, string url, AuthToken token, string userName = null, string password = null, Dictionary<string, string> parameters = null)
     {
-        string result = string.Empty;
-        HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
-        request.Method = method.Method;
+        //string result = string.Empty;
+        //HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+        //request.Method = method.Method;
+        //if (userName != null && password != null)
+        //{
+        //    string encoded =
+        //        Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(userName + ":" + password));
+        //    request.Headers.Add("Authorization", "Basic " + encoded);
+        //}
+        //else if (token != null)
+        //{
+        //    request.Headers.Add("Authorization", "Bearer " + token.access_token);
+        //}
+
+        //HttpWebResponse responce = (HttpWebResponse)request.GetResponse();
+        //using (StreamReader reader = new StreamReader(responce.GetResponseStream(), Encoding.UTF8))
+        //{
+        //    result = reader.ReadToEnd();
+        //}
+
+        //return result;
+
+        WebClient client = new WebClient();
         if (userName != null && password != null)
         {
             string encoded =
-                Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(userName + ":" + password));
-            request.Headers.Add("Authorization", "Basic " + encoded);
+                    Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(userName + ":" + password));
+            client.Headers.Add("Authorization", "Basic " + encoded);
         }
         else if (token != null)
         {
-            request.Headers.Add("Authorization", "Bearer " + token.access_token);
+            client.Headers.Add("Authorization", "Bearer " + token.access_token);
         }
 
-        HttpWebResponse responce = (HttpWebResponse)request.GetResponse();
-        using (StreamReader reader = new StreamReader(responce.GetResponseStream(), Encoding.UTF8))
+        if (parameters != null)
         {
-            result = reader.ReadToEnd();
+            foreach (var key in parameters.Keys)
+            {
+                client.QueryString.Add(key, parameters[key]);
+            }
         }
 
-        return result;
+        byte[] data = Array.Empty<byte>();
+        if (method == HttpMethod.Post)
+        {
+            data = client.UploadValues(url, method.Method, client.QueryString);
+        }
+        if (method == HttpMethod.Get)
+        {
+            data = client.DownloadData(url);
+        }
+
+        return Encoding.UTF8.GetString(data);
     }
 
     protected HttpStatusCode SendDataByUrl(HttpMethod method, string url, AuthToken token, string data = null)
--- TaskManager.Client/Services/DesksRequestService.cs
+++ TaskManager.Client/Services/DesksRequestService.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using TaskManager.Client.Models;
+using TaskManager.Common.Models;
 
 namespace TaskManager.Client.Services;
 
-public class DesksRequestService
+public class DesksRequestService : CommonRequestService
 {
+    private const string _desksControllerUrl = HOST + "desks";
 
+    public List<DeskModel> GetAllDesks(AuthToken token)
+    {
+        string responce = GetDataByUrl(HttpMethod.Get, _desksControllerUrl, token);
+        return JsonConvert.DeserializeObject<List<DeskModel>>(responce);
+    }
+
+    public DeskModel GetDeskById(AuthToken token, int deskId)
+    {
+        var result = GetDataByUrl(HttpMethod.Get, _desksControllerUrl + $"/{deskId}", token);
+        return JsonConvert.DeserializeObject<DeskModel>(result);
+    }
+
+    public HttpStatusCode DeleteDesk(AuthToken token, int deskId)
+    {
+        return DeleteDataByUrl(_desksControllerUrl + $"/{deskId}", token);
+    }
+
+    public List<DeskModel> GetDesksByProject(AuthToken token, int projectId)
+    {
+        var parameters = new Dictionary<string, string>();
+        parameters.Add("projectId", projectId.ToString());
+
+        string responce = GetDataByUrl(HttpMethod.Get, _desksControllerUrl + "/project", token, null, null, parameters);
+        return JsonConvert.DeserializeObject<List<DeskModel>>(responce);
+    }
+
+    public HttpStatusCode CreateDesk(AuthToken token, DeskModel desk)
+    {
+        string deskJson = JsonConvert.SerializeObject(desk);
+        return SendDataByUrl(HttpMethod.Post, _desksControllerUrl, token, deskJson);
+    }
+
+    public HttpStatusCode UpdateDesk(AuthToken token, DeskModel desk)
+    {
+        string deskJson = JsonConvert.SerializeObject(desk);
+        return SendDataByUrl(HttpMethod.Patch, _desksControllerUrl + $"/{desk.Id}", token, deskJson);
+    }
 }
